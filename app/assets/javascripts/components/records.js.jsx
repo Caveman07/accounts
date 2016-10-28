8544ca3197adc39f9e571de3f8f3b80d759c154d
caveman07 <ram07kbr90@gmail.com>
2016-10-28 13:29:09 +0300

    first commit

---
+++ app/assets/javascripts/components/records.js.jsx
@@ -0,0 +1,74 @@
+var Records = React.createClass({
+    getInitialState: function(){
+      return {records: this.props.data}
+    },
+
+    getDefaultProps: function(){
+      return {records: [] }
+    },
+
+    addRecord: function(record) {
+      var records = React.addons.update(this.state.records, {$push: [record]});
+      this.setState({ records: records });
+    },
+
+    credits: function(){
+      var credits = this.state.records.filter(function(val) {
+          return val.amount >= 0
+      });
+      return credits.reduce(function(prev, curr) {
+        return prev + parseFloat(curr.amount);
+      }, 0)
+
+    },
+
+    debits: function(){
+      var debits = this.state.records.filter(function(val) {
+        return val.amount < 0
+      });
+      return debits.reduce(function(prev, curr) {
+        return prev + parseFloat(curr.amount)
+      }, 0)
+    },
+    balance: function(){
+        return this.debits() + this.credits();
+    },
+
+    render: function() {
+      return(
+        <div>
+          <div className='records'>
+             <h2 className='title'>
+               Records
+             </h2>
+
+             <div className='row'>
+                  <AmountBox type='success' amount={this.credits()} text='Credit' />
+                  <AmountBox type='danger' amount={this.debits()} text='Debit' />
+                  <AmountBox type='info' amount={this.balance()} text='Balance' />
+             </div>
+
+             <RecordForm handleNewRecord={this.addRecord} />
+          </div>
+
+          <table className='table table-bordered'>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Title</th>
+                  <th>Amount</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {this.state.records.map(function(record) {
+                  return <Record key={record.id} record={record} />
+                  }.bind(this)
+                 )}
+              </tbody>
+        </table>
+      </div>
+      )
+    }
+});
