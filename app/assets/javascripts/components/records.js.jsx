c4988316e1daaae4ac2da9ccb2b1182dc85e6483
caveman07 <ram07kbr90@gmail.com>
2016-10-28 16:58:42 +0300

    working react features

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -12,6 +12,18 @@ var Records = React.createClass({
       this.setState({ records: records });
     },
 
+    updateRecord: function(record, data) {
+      var index = this.state.records.indexOf(record);
+      var records = React.addons.update(this.state.records, { $splice: [[index,1,data]] });
+      this.replaceState({records: records});
+    },
+
+    deleteRecord: function(record){
+      var index = this.state.records.indexOf(record);
+      var records = React.addons.update(this.state.records, {$splice: [[index,1]]});
+      this.replaceState({records: records});
+    },
+
     credits: function(){
       var credits = this.state.records.filter(function(val) {
           return val.amount >= 0
@@ -36,8 +48,7 @@ var Records = React.createClass({
 
     render: function() {
       return(
-        <div>
-          <div className='records'>
+        <div className='records'>
              <h2 className='title'>
                Records
              </h2>
@@ -49,26 +60,29 @@ var Records = React.createClass({
              </div>
 
              <RecordForm handleNewRecord={this.addRecord} />
+             <hr></hr>
+
+
+              <table className='table table-bordered'>
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>Title</th>
+                      <th>Amount</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {this.state.records.map(function(record) {
+                      return <Record key={record.id} record={record}
+                              handleDeleteRecord={this.deleteRecord}
+                              handleEditRecord={this.updateRecord}/>
+                      }.bind(this)
+                     )}
+                  </tbody>
+            </table>
           </div>
-
-          <table className='table table-bordered'>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Title</th>
-                  <th>Amount</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {this.state.records.map(function(record) {
-                  return <Record key={record.id} record={record} />
-                  }.bind(this)
-                 )}
-              </tbody>
-        </table>
-      </div>
       )
     }
 });
